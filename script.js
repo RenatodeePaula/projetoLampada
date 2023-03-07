772d2f93563bc37e517e4d7e5e29e09826fa368d
Renato de Paula <renatodepaula.tec@hotmail.com>
2023-03-07 14:53:36 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let miniBox = document.getElementById('mini-box')
+let button = document.getElementById('button')
+let lampada = document.querySelector('img')
+
+miniBox.addEventListener("click", onOff)
+
+
+
+function onOff() {
+    if(button.classList.contains('on')){
+        button.classList.remove('on')
+        button.classList.add('off')
+
+        lampada.setAttribute('src', './assets/ligada.jpg')
+
+        
+
+} else {
+    button.classList.remove('off')
+    button.classList.add('on')
+    lampada.setAttribute('src', './assets/desligada.jpg')
+
+}
+}
+
+
+lampada.addEventListener('dblclick', quebrar)
+
+function quebrar() {
+    lampada.setAttribute('src', './assets/quebrada.jpg')
+
+    if(lampada.attr(src="./assets/desligada.jpg") !== undefined) {
+        function onOff() {
+            if(button.classList.contains('on')){
+                button.classList.remove('on')
+                button.classList.add('off')
+        
+                lampada.setAttribute('src', './assets/ligada.jpg')
+        
+                
+        
+        } else {
+            button.classList.remove('off')
+            button.classList.add('on')
+            lampada.setAttribute('src', './assets/desligada.jpg')
+        
+        }
+        }
+    }
+}
