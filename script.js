a4c8ba01f87f0720520655512fa61eb5f358ddea
Renato de Paula <renatodepaula.tec@hotmail.com>
2023-03-08 14:06:48 -0300

    finish

--- script.js
+++ script.js
@@ -5,28 +5,53 @@ const body = document.body
 
 miniBox.addEventListener("click", onOff)
 
+function turnLampOff() {
+    button.classList.remove('on')
+    button.classList.add('off')
+
+    lampada.src = './assets/ligada.jpg'
+    body.style.background = "white"
+} 
+function turnLampOn() {
+    button.classList.remove('off')
+    button.classList.add('on')
+    lampada.src = 'assets/desligada-removebg-preview.png'
+    
+    body.style.background = "black"
+}
+
 function onOff() {
     if (lampada.classList.contains('quebrada')){
         return
     } else if (button.classList.contains('on')){
-        button.classList.remove('on')
-        button.classList.add('off')
-
-        lampada.setAttribute('src', './assets/ligada.jpg')  
-        body.style.background = "white"
-
+       turnLampOff()
     } else {
-        button.classList.remove('off')
-        button.classList.add('on')
-        lampada.setAttribute('src', 'assets/desligada-removebg-preview.png')
-        
-        body.style.background = "black"
+        turnLampOn()
     }
 } 
 lampada.addEventListener('dblclick', quebrar)
 
 function quebrar() {    
-    lampada.setAttribute('src', './assets/quebrada-removebg-preview.png')
+    lampada.src = './assets/quebrada-removebg-preview.png'
     lampada.classList.add('quebrada')
     body.style.background = "black"    
+}
+
+lampada.addEventListener("mouseover", mouseIn)
+
+function mouseIn (){
+    if (lampada.classList.contains('quebrada')) {
+        return
+    } else if (button.classList.contains('on')) {
+        turnLampOff()
+    }    
+}
+lampada.addEventListener("mouseleave", mouseOut)
+
+function mouseOut() {
+    if (lampada.classList.contains('quebrada')) {
+        return
+    } else if (button.classList.contains('off')) {
+        turnLampOn()
+    }
 }
