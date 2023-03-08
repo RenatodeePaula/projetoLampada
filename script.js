a74ec9376613b464067929e4ee9d483e3412ab8c
Renato de Paula <renatodepaula.tec@hotmail.com>
2023-03-07 22:58:06 -0300

    melhorias js

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
 let miniBox = document.getElementById('mini-box')
 let button = document.getElementById('button')
 let lampada = document.querySelector('img')
+const body = document.body
 
 miniBox.addEventListener("click", onOff)
 
 function onOff() {
-
-    if(button.classList.contains('on')){
+    if (lampada.classList.contains('quebrada')){
+        return
+    } else if (button.classList.contains('on')){
         button.classList.remove('on')
         button.classList.add('off')
 
-        lampada.setAttribute('src', './assets/ligada.jpg')     
+        lampada.setAttribute('src', './assets/ligada.jpg')  
+        body.style.background = "white"
 
     } else {
         button.classList.remove('off')
         button.classList.add('on')
-        lampada.setAttribute('src', './assets/desligada.jpg')
-
-}
+        lampada.setAttribute('src', 'assets/desligada-removebg-preview.png')
+        
+        body.style.background = "black"
+    }
 } 
 lampada.addEventListener('dblclick', quebrar)
 
 function quebrar() {    
-    lampada.setAttribute('src', './assets/quebrada.jpg')
+    lampada.setAttribute('src', './assets/quebrada-removebg-preview.png')
     lampada.classList.add('quebrada')
-
-    if (lampada.classList.contains('quebrada')) {
-
-         
-    } else {
-        miniBox.addEventListener("click", onOff)
-        
-    }
-
-    
+    body.style.background = "black"    
 }
